42ebe7f386b7228bd4bf7a56ecc505947cd6d867
Franziska Schulz <Franziska.Schulz@mpi.nl>
2022-03-15 12:54:57 +0100

    Added readme file

---
+++ Session1.py
@@ -0,0 +1,37 @@
+print("Hello")
+
+
+#Course overview
+#Preparation (Session 0)
+#Introduction (Session 1)
+#Stimulus preparation (visual and auditory) -> 2a and 2b
+#Experiment programming (Session 3 and 4)
+#Data preprocessing & viz (Session 5)
+#Neuroimaging processing (MEG, EEG/ fMRI) -> 6a and 6b
+#Statistics (Session 7)
+
+#for credits: 7 out of 10 sessions
+
+#Homework: build kind of a longer experiment with some analysis
+#Summary of the whole course
+#some homework is disconnected from the bigger project
+
+#Version management using GIT
+#Why version mangagement / Why GIT?
+#- keep track of everything you have done
+#- ability to revert to older (working) versions
+#- ability to check which change was made by whom
+
+#GIT
+# clean overview of all changes ever made to a project, decide which files to add to a project, decide when you commit your changes
+# synchronise with a remote server to share your project, built-in tools to merge conflicts created by collaborating, option to branch out and develop in isolation
+# always possible to revert to an earleier (commited!) state of the project  
+
+
+# Local repositories
+
+# windows power shell open: in einem Fenster: alt d und dann öffnen
+
+
+#git init . (dot for the current folder)
+#set folder in power shell: cd .\Session1\ (zum Beispiel)
